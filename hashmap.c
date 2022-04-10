0ac23fd5413b1f208b3cfdb8caec3a280d73f4bd
nicolasmery58gm <>
2022-04-10 23:03:39 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -77,13 +77,15 @@ Pair * searchMap(HashMap * map,  char * key) {
 
 Pair * firstMap(HashMap * map) {
   if (map == NULL) return NULL;
-  for(long i = 0; i < map->capacity; i++){
+  long primero;
+  for(int i = 0; i < map->capacity; i++){
     if(map->buckets[i]!= NULL){
       map->current = i;
+      primero=map->buckets[i];
       break;
     }
   }
-  return map->buckets[i];
+  return primero;
 }
 
 Pair * nextMap(HashMap * map) {
