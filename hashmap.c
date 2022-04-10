6f93e8919821843ac35d0a0ee439acae273f1a83
nicolasmery58gm <>
2022-04-10 23:52:42 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -76,9 +76,9 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
-  Pair* primero;
+  Pair** primero;
   for(long i; i < map->capacity; i++){
-    if(map->buckets[i]!= NULL){
+    if(map->buckets[i] != NULL){
       map->current = i;
       primero = map->buckets[i];
       break;
