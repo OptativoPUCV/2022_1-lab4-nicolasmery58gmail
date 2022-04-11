23a40d22d7f55cf64fb66aa056846fd01c6a0285
nicolasmery58gm <>
2022-04-11 02:55:31 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -101,7 +101,6 @@ Pair * firstMap(HashMap * map) {
 
 Pair * nextMap(HashMap * map) {
   long aux = map->current;
-  if (aux == map->capacity-1) return NULL;
   while(map->buckets[aux] == NULL || map->buckets[aux]->key == NULL){
     
     aux = (aux+1)%map->capacity;
