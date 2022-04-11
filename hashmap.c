b4c90000a285a093960844463eb3c5ae0d0c29e7
nicolasmery58gm <>
2022-04-11 02:32:53 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -40,8 +40,12 @@ int is_equal(void* key1, void* key2){
 
 
 void insertMap(HashMap * map, char * key, void * value) {
-  
-  
+  posicion = hash(key, map->capacity);
+  while(map->buckets[posicion]!=NULL || map->buckets[posicion]->key!=NULL){
+    posicion=(posicion+1)%map->capacity;
+  }
+  map->buckets[posicion]->key = key;
+  map->buckets[posicion]->value = value;  
 
 }
 
